5c113fb6a51b0b9f8ecda33102ade076ba28aa90
nut-natthawat <dfnmutthor@gmail.com>
2024-06-03 14:36:23 +0700

    kuy

--- CSS121/final/cypto.cpp
+++ CSS121/final/cypto.cpp
@@ -2,27 +2,30 @@
 #include <vector>
 #include <climits>
 #include <tuple>
+#include <array>
 using namespace std;
-
-tuple<int, vector<tuple<int, int, int>>> maxProfit(vector<int>& prices, int k) {
+struct Transaction {
+    int buyDay;
+    int sellDay;
+    int profit;
+};
+tuple<int, array<Transaction, 3>> maxProfit(const vector<int>& prices, int k) {
     int n = prices.size();
     int maxProfit = 0;
-    vector<tuple<int, int, int>> bestTransactions;
-
-    // ลองทุกความเป็นไปได้ของการซื้อขาย 3 ครั้ง
-    for (int i = 0; i < n; i++) {  //ซื้อครั้งแรก
-        for (int j = i + 1; j < n; j++) { // ขายครั้งแรก ต้องเป็นวันที่หลังจากวันที่ซื้อครั้งแรก
+    array<Transaction, 3> bestTransactions;
+    for (int i = 0; i < n; i++) { // ซื้อครั้งที่ 1
+        for (int j = i + 1; j < n; j++) { // ขายครั้งที่ 1 ต้องเป็นวันที่หลังจากวันที่ซื้อครั้งแรก
             for (int x = j + 1; x < n; x++) { // ซื้อครั้งที่ 2 ต้องเป็นวันที่หลังจากวันที่ขายครั้งแรก
-                for (int y = x + 1; y < n; y++) { //ขายครั้งที่ 2 ต้องเป็นวันที่หลังจากวันที่ซื้อครั้งที่สอง
+                for (int y = x + 1; y < n; y++) { // ขายครั้งที่ 2 ต้องเป็นวันที่หลังจากวันที่ซื้อครั้งที่สอง
                     for (int p = y + 1; p < n; p++) { // ซื้อครั้งที่ 3 ต้องเป็นวันที่หลังจากวันที่ขายครั้งที่สอง
-                        for (int q = p + 1; q < n; q++) { //-ขายครั้งที่ 3 ต้องเป็นวันที่หลังจากวันที่ซื้อครั้งที่สาม
-                            int profit1 = prices[j] - prices[i]; //กำไรครั้งแรก
-                            int profit2 = prices[y] - prices[x]; //กำไรครั้งที่ 2
-                            int profit3 = prices[q] - prices[p]; //กำไรครั้งที่ 3
-                            int totalProfit = profit1 + profit2 + profit3;
-                            if (totalProfit > maxProfit) {
-                                maxProfit = totalProfit;
-                                bestTransactions = {{i, j, profit1}, {x, y, profit2}, {p, q, profit3}};
+                        for (int q = p + 1; q < n; q++) { // ขายครั้งที่ 3 ต้องเป็นวันที่หลังจากวันที่ซื้อครั้งที่สาม
+                            int profit1 = prices[j] - prices[i]; // กำไร1
+                            int profit2 = prices[y] - prices[x]; // กำไร2
+                            int profit3 = prices[q] - prices[p]; // กำไร3
+                            int totalProfit = profit1 + profit2 + profit3; 
+                            if (totalProfit > maxProfit) { 
+                                maxProfit = totalProfit; 
+                                bestTransactions = {Transaction{i, j, profit1}, Transaction{x, y, profit2}, Transaction{p, q, profit3}}; // อัพเดตการซื้อขายที่ดีที่สุด
                             }
                         }
                     }
@@ -30,16 +33,16 @@ tuple<int, vector<tuple<int, int, int>>> maxProfit(vector<int>& prices, int k) {
             }
         }
     }
-
     return {maxProfit, bestTransactions};
 }
-
 int main() {
     vector<int> prices = {99, 2, 15, 17, 6, 35, 64, 10, 28, 32, 45, 11, 2, 36, 35, 44, 66, 9, 18, 24, 25, 98, 97, 11, 18, 2, 9, 19, 56, 12};
-    int k = 3; // จำนวนครั้งที่สามารถซื้อขายได้
+    int k = 3; 
     auto [max_profit, transactions] = maxProfit(prices, k);
-    for (const auto& trans : transactions) {
-        cout << "buy: " << get<0>(trans) + 1 << " sell: " << get<1>(trans) + 1 << " profit: " << get<2>(trans) << endl; 
-    }
     cout << "maxprofit: " << max_profit << endl;
+    for (const auto& trans : transactions) {
+        cout << "buy: " << trans.buyDay + 1 << " sell: " << trans.sellDay + 1 
+             << " profit: " << trans.profit << endl;
+    }
+    return 0;
 }
