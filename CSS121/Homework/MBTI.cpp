14aee1824edac4ddfd4fbd71ba7809ace0e8be99
nut-natthawat <dfnmutthor@gmail.com>
2024-06-03 15:09:30 +0700

    asdasd

--- CSS121/Homework/MBTI.cpp
+++ CSS121/Homework/MBTI.cpp
@@ -93,36 +93,10 @@ int main() {
     Data_friend[22] = {"Natthapol Premkamon", 25.4, 26, 18, 28, 25, 23, 19, 23, "ISTP"};
     Data_friend[23] = {"Amy Suganya Tripp", 20, 14.8, 31, 37, 33, 17, 24, 27, "ESTP"};
     Data_friend[24] = {"Nichanun Boontanom", 24.2, 25.6, 21, 26, 23, 28, 15, 21, "ISTP"};
-
-    /*
-    Data_friend[25] = {"Poomthai Promkote", 39.6, 46.8, 33, 36, 32, 36, 30, 30, "INTP"};
-    Data_friend[26] = {"Lalhada Suttiprapha", 27.8, 23.4, 21, 27, 24, 35, 26, 22, "ISFJ"};
-    Data_friend[27] = {"Santijit Kamnak", 22.4, 20.6, 26, 26, 26, 30, 22, 25, "ISTJ"};
-    Data_friend[28] = {"Kantinan Kuikeaw", 13.8, 16, 33, 31, 35, 32, 23, 16, "ESTJ"};
-    Data_friend[29] = {"Chothanin Thitisrirat", 29.8, 28, 24, 26, 29, 32, 36, 18, "ESFJ"};
-    Data_friend[30] = {"Warin Wattanapornprom", 32, 32, 27, 36, 29, 31, 28, 23, "INTP"};
-    Data_friend[31] = {"Yanisa Mungkarotai", 23, 26.6, 28, 25, 17, 29, 29, 31, "INTJ"};
-    Data_friend[32] = {"Jessada Pranee", 27.8, 24.2, 32, 29, 18, 20, 13, 31, "INTP"};
-    Data_friend[33] = {"Suphanut Chanroong", 34.6, 32, 25, 29, 23, 24, 29, 26, "ENFP"};
-    Data_friend[34] = {"Keerataphan Malai", 15.6, 28.2, 21, 31, 19, 28, 18, 23, "ISTP"};
-    Data_friend[35] = {"Thanachot Wongyai", 24.8, 19.2, 25, 30, 22, 28, 19, 25, "INTP"};
-    Data_friend[36] = {"Pawinnarut Pornpanarat", 47.4, 31.2, 36, 32, 36, 37, 29, 31, "ENTP"};
-    Data_friend[37] = {"Phirada Chemmanmud", 23.6, 22.2, 28, 35, 27, 40, 23, 22, "ISTJ"};
-    Data_friend[38] = {"Phattawee Witthayapanyanont", 37.2, 23.6, 36, 39, 34, 33, 31, 16, "INTP"};
-    Data_friend[39] = {"Sathapana Tinop", 23.2, 30.6, 30, 36, 19, 21, 4, 28, "INTP"};
-    Data_friend[40] = {"Nuttasit Tannitipaisal", 33.6, 34.4, 24, 24, 31, 35, 32, 25, "ENTP"};
-    Data_friend[41] = {"Nut Somwang", 19.6, 19.2, 25, 32, 36, 30, 20, 17, "ESTJ"};
-    Data_friend[42] = {"Pattarachanon Uraiwichaikul", 35.6, 28.4, 36, 38, 36, 28, 29, 30, "ESTJ"};
-    Data_friend[43] = {"Pattarapong Srisawat", 32.4, 28.4, 32, 32, 28, 28, 28, 28, "ENTP"};
-    Data_friend[44] = {"Krantharat Ratchart", 26.6, 21, 27, 27, 25, 26, 22, 19, "ESTJ"};
-    Data_friend[45] = {"Pasin Laopooti", 32.8, 31, 28, 37, 29, 38, 25, 29, "INTP"};
-    Data_friend[46] = {"Patchnida Hemwannanukul", 29.8, 23.4, 30, 26, 27, 30, 29, 29, "ESTJ"};
-    Data_friend[47] = {"Nakamon Yongpaisarn", 28, 24, 31, 31, 29, 37, 28, 28, "INFP"};
-    Data_friend[48] = {"Natthapon Tanateeraanan", 24.6, 22, 32, 32, 36, 38, 35, 27, "ESTP"};
-    Data_friend[49] = {"Naphatchanun Suecey",23.4, 26, 31, 33, 24, 27, 31, 21, "ESFJ"};
-
-    */
-   Data_me = {"Natthawat Noiauthai", 23.4, 27,26,25,27,30,23,20,""}; //ISTP
+    Data_friend[25] = {"Piriyakorn Srisook", 31.6, 29.4, 32, 21, 28, 33, 30, 21, "ENFJ"};
+    Data_friend[26] = {"Napadech Kaewnok", 24.6, 32.2, 31, 39, 31, 35, 21, 29, "ISTP"};
+    Data_friend[27] = {"Pongsatorn Waiyaworn", 21.2, 38.8, 32, 36, 29, 35, 33, 30, "INTJ"};
+    Data_me = {"Natthawat Noiauthai", 23.4, 27,26,25,27,30,23,20,""}; //ISTP
 
     knn(Data_friend, Data_me);
     return 0;
