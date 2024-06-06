6f9483035e090c51e8b30eaf605f28697a5b67df
nut-natthawat <dfnmutthor@gmail.com>
2024-06-06 13:13:57 +0700

    asdasd

--- CSS121/Bonus/sorting.cpp
+++ CSS121/Bonus/sorting.cpp
@@ -1,65 +1,66 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-struct Row
-{
+
+struct Row {
     int A;
     int B;
     int C;
 };
 
-void insertionsort(std::vector<Row>&arr, int n,char columm){
-    for (size_t i = 1; i < n; ++i) {
+void insertionsort(Row arr[], int n, char primary, char secondary, char tertiary) {
+    for (int i = 1; i < n; ++i) {
         Row key = arr[i];
-        size_t j = i;
-        switch (columm) {
-            case 'A':
-                while (j > 0 && arr[j - 1].A > key.A) {
-                    arr[j] = arr[j - 1];
-                    --j;
+        int j = i - 1;
+        while (j >= 0) {
+            bool shouldSwap = false;
+            if (primary == 'A') {
+                if (arr[j].A > key.A ||
+                   (arr[j].A == key.A && arr[j].B > key.B) ||
+                   (arr[j].A == key.A && arr[j].B == key.B && arr[j].C > key.C)) {
+                    shouldSwap = true;
                 }
-                break;
-            case 'B':
-                while (j > 0 && arr[j - 1].B > key.B) {
-                    arr[j] = arr[j - 1];
-                    --j;
+            } else if (primary == 'B') {
+                if (arr[j].B > key.B ||
+                   (arr[j].B == key.B && arr[j].C > key.C) ||
+                   (arr[j].B == key.B && arr[j].C == key.C && arr[j].A > key.A)) {
+                    shouldSwap = true;
                 }
-                break;
-            case 'C':
-                while (j > 0 && arr[j - 1].C > key.C) {
-                    arr[j] = arr[j - 1];
-                    --j;
+            } else if (primary == 'C') {
+                if (arr[j].C > key.C ||
+                   (arr[j].C == key.C && arr[j].A > key.A) ||
+                   (arr[j].C == key.C && arr[j].A == key.A && arr[j].B > key.B)) {
+                    shouldSwap = true;
                 }
-                break;
-            default:
-                break;
+            }
+            if (!shouldSwap) break;
+            arr[j + 1] = arr[j];
+            --j;
         }
-        arr[j] = key;
+        arr[j + 1] = key;
     }
 }
-int main(){
-    int n = 15;
-    std::vector<Row> data ={
-        {1,1,4},
-        {3,1,1},
-        {4,4,4},
-        {2,4,4},
-        {3,5,3},
-        {4,3,3},
-        {1,3,3},
-        {2,4,3},
-        {3,3,5},
-        {1,5,3},
-        {1,1,4},
-        {4,1,1},
-        {5,2,3},
-        {3,5,2}
+
+int main() {
+    const int n = 14;
+    Row data[n] = {
+        {1, 1, 4}, {3, 1, 1}, {4, 4, 4}, {2, 4, 4}, {3, 5, 3}, {4, 3, 3},
+        {1, 3, 3}, {2, 4, 3}, {3, 3, 5}, {1, 5, 3}, {1, 1, 4}, {4, 1, 1},
+        {5, 2, 3}, {3, 5, 2}
     };
-    char columm;
-    cout << "Input columm to sort (A,B,C) : ";
-    cin >> columm;
-    insertionsort(data,n,columm);
-    for(const Row& row : data){
-        cout << row.A << " " << row.B << " " << row.C << endl;
+
+    char primary, secondary, tertiary;
+    cout << "Input primary column to sort (A,B,C): ";
+    cin >> primary;
+    cout << "Input secondary column to sort (A,B,C): ";
+    cin >> secondary;
+    cout << "Input tertiary column to sort (A,B,C): ";
+    cin >> tertiary;
+
+    insertionsort(data, n, primary, secondary, tertiary);
+
+    for (int i = 0; i < n; ++i) {
+        cout << data[i].A << " " << data[i].B << " " << data[i].C << endl;
     }
+
+    return 0;
 }
